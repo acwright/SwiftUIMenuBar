9255b26bfba1f6df01dc7c63b836bf02a087f4bf
Aaron Wright <aaron.c.wright@icloud.com>
2019-12-18 10:53:32 -0600

    Initial Commit

---
+++ SwiftUIMenuBar/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIMenuBar
+//
+//  Created by Aaron Wright on 12/18/19.
+//  Copyright © 2019 Aaron Wright. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
